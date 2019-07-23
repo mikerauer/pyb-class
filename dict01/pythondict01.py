f4d3dfb923eb6d8dcc92ab625d757dbcc62df92c
mikerauer <mikerauer@gmail.com>
2019-07-23 10:18:02 -0400

    dict lab

---
+++ dict01/pythondict01.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+##create a dictionary
+switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
+
+##display parts of the dictionary
+print(switch['hostname'])
+print(switch['ip'])
+
+##request fake key
+#print(switch['lynx'])
+
+##request a fake key with .get() method
+print("First test - .get()")
+print(switch.get('lynx'))
+
+print("Second test - .get()")
+print(switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE"))
+
+print ("Third test - .get()")
+print(switch.get('version'))
+
+print("Fourth test - .get()")
+print(switch.keys())
+
+print("Fifth test - .get()")
+print(switch.values())
+
+print("Sixth test - .pop()")
+switch.pop('version') #removes this key (and value) pair
+print(switch.keys()) # value of versions gone
+print(switch.values()) #notice the value of 1.2
+
+print("Seventh test - ADD a new value")
+switch['adminlogin'] = 'karl08'
+print(switch.keys()) 
+print(switch.values())
+
+print("Eighth test - ADD a new value")
+switch['password'] = 'qwerty'
+print(switch.keys()) 
+print(switch.values())
+
+
