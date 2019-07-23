c4f6bf84cc1ba6fb473ac8ad2f220ca8210c44a0
mikerauer <mikerauer@gmail.com>
2019-07-23 11:49:41 -0400

    def func

---
+++ strings/stringmethod01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# create a small string
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ") # this returns a list
+print(newlist)
+
+# create a list of strings
+myiplist = ["192", "168", "0", "12"]
+# set singleip as the result of .join around "."
+singleip = ".".join(myiplist)
+print(singleip)
