5f293cc6b36914f966ca38d5eedbc88e6369789d
mikerauer <mikerauer@gmail.com>
2019-07-23 15:12:42 -0400

    If statements

---
+++ if-test/condition01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+hostname = input("What value should we set for hostname?")
+#hostname = "MTG"
+
+hostname = hostname.upper()
+
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
+
